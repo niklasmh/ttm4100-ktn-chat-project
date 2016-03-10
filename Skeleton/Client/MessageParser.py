e84c13cdbc4ae9f2cd334c17ac6bd3ad51aa9708
alexanws <alex.wennevold@gmail.com>
2016-03-10 13:10:36 +0100

    removed syntax errors

--- Skeleton/Client/MessageParser.py
+++ Skeleton/Client/MessageParser.py
@@ -8,15 +8,15 @@ class MessageParser():
             'info': self.parse_info,
             'login': self.parse_login, #ting jeg tror skal med herfra og ned
             'logout': self.parse_logout,
-            'names': self.parse_names;
-            'help': self.parse_help:
-            'history': self_parse_history;
-            'message': self_parse_message;
+            'names': self.parse_names,
+            'help': self.parse_help,
+            'history': self_parse_history,
+            'message': self_parse_message
 	    # More key:values pairs are needed	
         }
 
     def parse(self, payload):
-        payload = # decode the JSON object
+        #payload = # decode the JSON object
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
@@ -40,3 +40,4 @@ class MessageParser():
 
     def parse_message(self, payload);
     # Include more methods for handling the different responses... 
+
